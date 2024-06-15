9a179480f5edaa01afca53084572ceb33bd59ebd
Steven Gutierrez <stevogbmv@gmail.com>
2024-06-14 21:57:09 -0700

    layout pages

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -5,7 +5,7 @@ const Services = () => {
     <div className="h-[100vh] flex bg-blue-900">
         <section className="w-full py-32 md:py-48">
             <div className='flex justify-center'>
-                <h2 className="text-4xl font-bold text-white bg-blue-900 opacity-70">Services</h2>
+                <h2 className="text-4xl font-bold text-white">Services</h2>
             </div>
         </section>
     </div>
--- src/components/stats/index.tsx
+++ src/components/stats/index.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='flex bg-white h-[20vh] justify-center'>
+        <section className='flex flex-row items-center mx-auto'>
+            <div className={boxStyle}>1</div>
+            <div className={boxStyle}>2</div>
+            <div className={boxStyle}>3</div>
+        </section>
+    </div>
   )
 }
 
+const boxStyle = ' border border-black p-10 mx-9 sm:mx-16 md:mx-24 lg:32'
 export default Stats
