387881b2f1631d96902df33baa2ab8b62ee0637c
Steven Gutierrez <stevogbmv@gmail.com>
2024-06-23 14:12:35 -0700

    updated hero, included anchor tag linking to contact section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,12 +1,17 @@
 export const Hero = () => {
 
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-slate-950">
-      <div className='flex justify-center items-center h-[50vh] border border-black '>
+    <div className="flex flex-col sm:flex-row min-h-[90vh] bg-white dark:bg-slate-950">
+      <div className='flex justify-center items-center h-[50vh]'>
         <img className='object-contain' src="/GabrielCarCleaning.jpeg" alt="" />
       </div>
-      <div className='flex my-10 mx-auto text-4xl text-teal-950'>
-        <p>Perfection in Every Detail</p>
+      <div className='flex flex-col sm:w-[40vw] sm:items-center text-center sm:mt-16'>
+        <div className='flex my-10 mx-auto font-bold text-4xl text-Teal sm:mx-0 sm:mb-20'>
+          <p>Perfection in Every Detail</p>
+        </div>
+        <a href='#contact' className='bg-Teal mx-40 text-white rounded-full py-2 sm:w-32'>
+          Contact Us
+        </a>
       </div>
     </div>
   )
