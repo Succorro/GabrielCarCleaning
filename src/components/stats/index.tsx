b0e7b766029c95582f3713d1316dbc6411589ccd
Steven Gutierrez <stevogbmv@gmail.com>
2024-07-07 14:06:06 -0700

    updated stats, inlcuded delay in stat transition

--- src/components/stats/index.tsx
+++ src/components/stats/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import React, { useRef } from "react";
 import { useTrail, animated as a } from "react-spring";
 import Stat from './stat';
-
+import useOnScreen from '../../hooks/useOnScreen';
 
 const config = { mass: 5, tension: 1000, friction: 200 };
-const Stats = () => {
-    const [toggle, set] = useState(true);
+
+const Stats: React.FC = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    const onScreen = useOnScreen(ref, '-50px');
+
     const statInfo = [
         {
             img: 'hour',
@@ -22,33 +25,35 @@ const Stats = () => {
             number: 100,
             text: '% Satisfaction Guarantee'
         }
-    ]
+    ];
+
     const trail = useTrail(statInfo.length, {
-      config,
-      opacity: toggle ? 1 : 0,
-      x: toggle ? 0 : 20,
-      height: toggle ? 80 : 0,
-      from: { opacity: 0, x: 20, height: 0 }
+        config,
+        opacity: onScreen ? 1 : 0,
+        transform: onScreen ? 'translate3d(0,0px,0)' : 'translate3d(0,50px,0)',
+        from: { opacity: 0, transform: 'translate3d(0,50px,0)' },
+        delay: 100
     });
-  return (
-    <div className='bg-white dark:bg-slate-400'>
-            <div className='flex flex-col sm:flex-row  items-center sm:justify-center ' onClick={() => set(state => !state)}>
-                    {trail.map(({ x, height, ...rest }, index) => (
+
+    return (
+        <div className='bg-white dark:bg-slate-400' ref={ref}>
+            <div className='flex flex-col sm:flex-row items-center sm:justify-center'>
+                {trail.map((style, index) => (
                     <a.div
                         key={statInfo[index].img}
                         className="w-full pb-10"
-                        style={{
-                        ...rest,
-                        transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
-                        }}
+                        style={style}
                     >
-                        <a.div style={{ height }}>{<Stat img={statInfo[index].img} number={statInfo[index].number} text={statInfo[index].text}/>}</a.div>
+                        <Stat 
+                            img={statInfo[index].img} 
+                            number={statInfo[index].number} 
+                            text={statInfo[index].text}
+                        />
                     </a.div>
-                    ))}
+                ))}
             </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default Stats
-
+export default Stats;
--- src/components/stats/stat.tsx
+++ src/components/stats/stat.tsx
@@ -1,38 +1,35 @@
-import { FaHourglassEnd } from "react-icons/fa"
-import AnimatedCounter from "../ui/AnimatedConter"
-import {IoCarSport} from 'react-icons/io5'
+import React from "react";
+import { FaHourglassEnd } from "react-icons/fa";
+import AnimatedCounter from "../ui/AnimatedConter";
+import { IoCarSport } from 'react-icons/io5';
 import { IoMdThumbsUp } from "react-icons/io";
 
-interface HeaderProps {
-    img: string,
-    number: number,
-    text: string
-}
-const Stat = ({img, number, text}: HeaderProps) => {
-    const image = () => {
-        if(img === 'hour'){
-            return (
-                <FaHourglassEnd size ='40px'/>
-            )
-        } else if(img === 'car'){
-            return (
-                <IoCarSport size='40px'/>
-            )
-        } else {
-            return(
-                <IoMdThumbsUp size='40px'/>
-            )
-        }
-    }
-  return (
-    <div className='flex flex-col items-center text-center text-Teal dark:text-DTeal font-semibold text-xl mx-auto my-5 py-5'>
-        {image()}
-        <div className="flex mt-3 ">
-            <AnimatedCounter end={number} duration={1000}/>
-            <p className="">{text}</p>
-        </div>
-    </div>
-  )
+interface StatProps {
+    img: string;
+    number: number;
+    text: string;
 }
 
-export default Stat
+const Stat: React.FC<StatProps> = ({ img, number, text }) => {
+    const image = () => {
+        if (img === 'hour') {
+            return <FaHourglassEnd size='40px' />;
+        } else if (img === 'car') {
+            return <IoCarSport size='40px' />;
+        } else {
+            return <IoMdThumbsUp size='40px' />;
+        }
+    };
+
+    return (
+        <div className='flex flex-col items-center text-center text-Teal dark:text-DTeal font-semibold text-xl mx-auto my-5 py-5'>
+            {image()}
+            <div className="flex mt-3 ">
+                <AnimatedCounter end={number} duration={1000} />
+                <p className="">{text}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Stat;
